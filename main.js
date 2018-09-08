d94e2f94407929afef313f9ab7a5a737ee94658e
Miguel Angel Franco <mfranco_98@yahoo.com>
2018-09-08 02:28:06 -0500

    se creó el html y un poco de js

---
+++ main.js
@@ -0,0 +1,43 @@
+
+//boton guardar
+let btnGuardar = document.getElementById("guardar");
+
+//input
+let curp = document.getElementById("curp");
+
+
+
+//Referencia a la base de datos,rama principal que es la rama "usuarios"
+const ref = firebase.database().ref("users");
+
+
+btnGuardar.addEventListener("click", ()=>{
+  //.value para obtener el valor del input
+  let objeto = {
+    curp: curp.value,
+  }
+  console.log(objeto);
+
+  ref.push(objeto).then(()=>{
+    alert("se subió");
+  }).catch((error)=>{
+    console.log(error);
+  })
+})
+
+
+//se jala el valor que hay en la base de datos..
+ref.on('value', (data)=>{
+
+  console.log("probando datas: ",data.val())
+
+  let dat = data.val()
+  let name = ""
+  //agarra el id como i
+  for(let i in dat){
+   console.log(dat[i].nombre);
+   name += dat[i].nombre + " ";
+  }
+  dataUsuario.innerHTML = name;
+
+})
