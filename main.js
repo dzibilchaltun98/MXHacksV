cba7c3a60a0c5fe4402763c525f2f699c570fbf9
Miguel Angel Franco <mfranco_98@yahoo.com>
2018-09-08 20:37:21 -0500

    Se agregó inicio de sesión con Firebase y se arregló un poco el html

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 //boton guardar
 let btnGuardar = document.getElementById("guardar");
+let btnSesion = document.getElementById("sesion");
+let btnCerrar = document.getElementById("cerrss");
 
 //input
 let nombre = document.getElementById("nombre");
@@ -11,6 +13,58 @@ let genero = document.getElementById("genero");
 
 //Referencia a la base de datos,rama principal que es la rama "usuarios"
 const ref = firebase.database().ref("users");
+
+
+btnSesion.addEventListener("click",function(){
+  console.log('click event')
+  
+  let provider = new firebase.auth.GoogleAuthProvider();
+  // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');  console.log("login");
+  
+  firebase.auth().signInWithPopup(provider).then((result)=>{
+    console.log(result.user);
+    
+  }).catch((error)=>{
+    console.log(error);
+  })
+  
+  alert("Se ha iniciado sesión")  
+});
+
+//Prueba de que el boton funciona
+firebase.auth().onAuthStateChanged(function(user){
+  console.log(user);
+  if(user){
+    console.log("hay usuario");
+    showLogout();
+  }else{
+    console.log("no hay usuario");
+    showLogin();
+  }
+});
+
+btnCerrar.addEventListener("click", ()=>{
+  
+  firebase.auth().signOut().then(()=>{
+    showLogin();
+    console.log("ya salí");
+  }).catch(()=>{    
+    showLogout();
+  })
+  alert("se ha cerrado la sesion");
+})
+
+//ocultar logout
+const showLogout = () =>{
+  btnSesion.style.display = "none";
+  btnCerrar.style.display = "block";
+}
+//ocultar login
+const showLogin = () =>{
+  btnSesion.style.display = "block";
+  btnCerrar.style.display = "none"
+}
+
 
 
 btnGuardar.addEventListener("click", ()=>{
@@ -36,14 +90,16 @@ btnGuardar.addEventListener("click", ()=>{
 //se jala el valor que hay en la base de datos..
 ref.on('value', (data)=>{
 
-  console.log("probando datas: ",data.val())  
+  //console.log("probando datas: ",data.val())  
 
   let dat = data.val()
   let name = ""
   //agarra el id como i
   for(let i in dat){
-   console.log(dat[i].nombre);
-   name += dat[i].nombre + " ";
+   //console.log(dat[i].algo)   se va a imprimir el algo
+    //console.log(i);
+   //
+   name = dat[i].nombre + " ";
   }
 
 })
